0723b109bd1e352e0046dde21f0cb1fcf27ec1a3
maxefimov <efimov.top.academy@gmail.com>
2023-09-07 19:24:55 +0300

    07-09-23

--- StlThreadsProject/StlThreadsProject.cpp
+++ StlThreadsProject/StlThreadsProject.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <thread>
 #include <mutex>
+#include <future>
 
 using namespace std;
 
@@ -27,6 +28,18 @@ void func()
     }
 }
 
+void sum100(promise<int> prom_pes)
+{
+    cout << "current thread id = " << this_thread::get_id() << "\n";
+
+    int sum{};
+    for (int i = 1; i <= 1000; i++)
+        sum += i;
+
+    this_thread::sleep_for(100ms);
+    prom_pes.set_value(sum);
+}
+
 int main()
 {
     //thread th1(inc);
@@ -42,8 +55,27 @@ int main()
     //th1.join();
     //th2.join();
 
-    jthread th1(inc, 100);
+    /*jthread th1(inc, 100);
     jthread th2(inc, 200);
 
-    cout << "amount = " << amount << "\n";
+    cout << "amount = " << amount << "\n";*/
+
+    cout << "main thread id = " << this_thread::get_id() << "\n";
+
+    /*future<int> result = async(sum100);
+
+    jthread th2(inc, 200);
+
+    result.wait();
+    cout << "result = " << result.get() << "\n";*/
+
+    promise<int> promise_result;
+    future<int> future_result = promise_result.get_future();
+
+    jthread th2(inc, 200);
+
+    jthread th3(sum100, move(promise_result));
+    future_result.wait();
+    cout << "result = " << future_result.get() << "\n";
+    
 }
